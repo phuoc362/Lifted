02cd26186e98e3f09477db478a9217ff37909df4
phuoc362 <phuoc362@gmail.com>
2019-05-17 00:43:24 +0700

    Update GITHUB.sh

--- script/GITHUB.sh
+++ script/GITHUB.sh
@@ -1,27 +1,97 @@
 
 
+####################################################################################
+########################    CpGs   #############################################
+####################################################################################
+####################################################################################
 
 
-
-
-
+mkdir input output process
 
 ########################################## input filename in bed file ; have 4 column with chr, start, end, value
+##########################################
+# for i in `ls *.bed *.bedGraph`; do echo $i
+# awk 'NF==4{print "input have 4 columns"}else {print "stop run"}' $i  ?
+# done
+
+cp *.bed *.bedGraph input/
+cd input
 ##########################################  length of CpGs =1
-for i in `ls filename`; do echo $i
-bedtool subtract -a <(sort $i) -b /path/to/file/gap.hg19.bed > temp
-liftOver temp /path/to/file/hg19ToHg38.over.chain  /path/output/file/name.output.bed /path/output/name_unliftOver
-bedtools subtract -a <(sort name.output.bed) -b /path/to/file/gap.hg38.bed > output.liftOvered
-done
 
 
-########################################## filtered
-for i in `ls output.liftOvered`; do echo $i
-sort -k1,1 -k2,2n $i|grep -E "chr(.|..)[[:blank:]]"|awk '{if($3-$2==1) print $0}'|awk '{print $0"\t"$1"."$2"."$3}'|sort -k5,5|uniq -f4 -u|sort -k1,1 -k2,2n|awk '{print $1"\t"$2"\t"$3"\t"$4}' > output.filtered.step1
+########################################## filter gap hg19 ++>  liftOver ++> filter gap hg38
+for i in `ls *.bed `; do echo $i
+bedtool subtract -a <(sort $i) -b process/gap.hg19.bed > temp
+liftOver temp /process/hg19ToHg38.over.chain  process/${i/.bed/.lo19to38.bed} ${i/.bed/.unliftOver}
+bedtools subtract -a <(sort ${i/.bed/.lo19to38.bed}) -b /process/gap.hg38.bed > ${i/.bed/.lo19to38.f1.bed}
+
+##########################################  filtered alt chr; duplicate
+sort -k1,1 -k2,2n ${i/.bed/.lo19to38.f1.bed}|grep -E "chr(.|..)[[:blank:]]"|awk '{if($3-$2==1) print $0}'|awk '{print $0"\t"$1"."$2"."$3}'|sort -k5,5|uniq -f4 -u|sort -k1,1 -k2,2n|awk '{print $1"\t"$2"\t"$3"\t"$4}' > ${i/.bed/.lo19to38.f2.bed}
 # filter not cg
-bedtools intersect -a output.filtered.step1 -b lo19to38.CpG.notcg.bed -v -wa -f 1 > output.filtered
+bedtools intersect -a ${i/.bed/.lo19to38.f2.bed} -b /process/lo19to38.CpG.notcg.bed -v -wa -f 1 > ${i/.bed/.lo19to38.filtered.bed}
 done
+
+###########
+rm *.lo19to38.f1.bed *.lo19to38.f2.bed
 ########### 
+cd ..
+mv input/*.lo19to38.bed  output/
+mv input/*.unliftOver	output/
+mv input/*.lo19to38.filtered.bed  output/
+
+
+
+
+
+
+
+####################################################################################
+########################    Chipsseq   #############################################
+####################################################################################
+####################################################################################
+
+mkdir input output process
+
+########################################## input filename in bed file ; have 4 column with chr, start, end, value
+##########################################
+# for i in `ls *.bed *.bedGraph`; do echo $i
+# awk 'NF==4{print "input have 4 columns"}else {print "stop run"}' $i  ?
+# done
+
+cp *.bed *.bedGraph input/
+cd input
+##########################################  create ID in column 5
+for i in `ls *.bed `; do echo $i
+awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$1"."$2"."$3}' > ${i/.bed/.chipseq.bed}
+
+done
+########################################## filter gap hg19 ++>  liftOver ++> filter gap hg38
+for i in `ls *.chipseq.bed `; do echo $i
+bedtool subtract -a <(sort $i) -b process/gap.hg19.bed > temp
+liftOver temp /process/hg19ToHg38.over.chain  process/${i/.bed/.lo19to38.bed} ${i/.bed/.unliftOver}
+bedtools subtract -a <(sort ${i/.bed/.lo19to38.bed}) -b /process/gap.hg38.bed > ${i/.bed/.lo19to38.f1.bed}
+
+########################################## filtered alt chr; duplicate
+bedtools merge -i  <(sort -k1,1 -k2,2n ${i/.bed/.lo19to38.f1.bed}|grep -E "chr(.|..)[[:blank:]]"|sort -k5,5|uniq -f4 -u|sort -k1,1 -k2,2n) -c 5 -o collapse -delim "_"|awk '{if($5!~"_"){print $0}}'|sort -k1,1 -k2,2n> ${i/.bed/.lo19to38.f2.bed}
+# filter not cg
+bedtools intersect -a ${i/.bed/.lo19to38.f2.bed} -b /process/lo19to38.CpG.notcg.bed -v -wa -f 1 > ${i/.bed/.lo19to38.filtered.bed}> ${i/.bed/.lo19to38.f2.bed}
+done
+
+########### remove
+rm *.lo19to38.f1.bed *.lo19to38.f2.bed
+########### transfer file to output
+cd ..
+mv input/*.lo19to38.bed  output/
+mv input/*.unliftOver	output/
+mv input/*.lo19to38.filtered.bed  output/
+
+
+
+
+
+
+
+
 
 
 
