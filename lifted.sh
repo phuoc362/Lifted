bd74f3200a6d027e5e7b12e21b5a3dfffcb87f28
phuoc362 <phuoc362@gmail.com>
2019-05-22 23:47:58 +0700

    Update lifted.sh

--- lifted.sh
+++ lifted.sh
@@ -13,7 +13,7 @@ outputunmap=$3
 awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$1"."$2"."$3}' $bedpath| sort -k1,1 -k2,2n  >temp0
 bedtools subtract -a temp0 -b hg19.bgap.tgap.and.blacklist.bed > temp
 ## lifftover
-./liftOver temp hg19ToHg38.over.chain temp1 $outputunmap
+liftOver temp hg19ToHg38.over.chain temp1 $outputunmap
 ### filter gap in hg38, remove in full CPGs
 sort -k1,1 -k2,2n temp1 > temp01
 sort -k1,1 -k2,2n hg19.between2un-gapped.bed > temp02
