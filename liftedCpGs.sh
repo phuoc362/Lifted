84a8f1ade47a34822ea424ba8e4bc8c8f34fdad6
phuoc362 <phuoc362@gmail.com>
2019-05-22 23:48:22 +0700

    Update liftedCpGs.sh

--- liftedCpGs.sh
+++ liftedCpGs.sh
@@ -15,7 +15,7 @@ outputunmap=$3
 sort -k1,1 -k2,2n $bedpath >temp0
 bedtools subtract -a temp0 -b hg19.bgap.tgap.and.blacklist.bed > temp
 ## lifftover
-./liftOver temp hg19ToHg38.over.chain temp1 $outputunmap
+liftOver temp hg19ToHg38.over.chain temp1 $outputunmap
 ### filter gap in hg38, remove in full CPGs
 sort -k1,1 -k2,2n temp1 > temp01
 cat remove.bed hg19.between2un-gapped.bed|sort -k1,1 -k2,2n > temp02
