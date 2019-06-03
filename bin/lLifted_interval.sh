1555411da9acf20057af13a033602de07e8417e1
phuoc362 <phuoc362@gmail.com>
2019-06-04 00:09:40 +0700

    Update lLifted_interval.sh

--- bin/lLifted_interval.sh
+++ bin/lLifted_interval.sh
@@ -2,28 +2,30 @@
 ########################    Chipseq  less conservative #############################################
 ################################################################################
 ################################################################################
-#### required  hg19.bgap.tgap.and.blacklist.bed file
-#### required  hg19.between2un-gapped.bed file
-#### required hg19ToHg38.over.chain file
+#### bedtools
 #### required liftOver tools
 
 ### input
 bedpath=$1
-outputmap=$2
-outputunmap=$3
+chainfile=$2
+outputmap=$3
+outputunmap=$4
 
 # format bed file with 4 column: chromosome_start_end_value
 awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$1"."$2"."$3}' $bedpath| sort -k1,1 -k2,2n  >temp0
 
 # filtered blacklist and gap(gapped-in-hg19, gapped-in-both and blacklist)
-bedtools subtract -a temp0 -b hg19.bgap.tgap.and.blacklist.bed > temp
+bedtools subtract -a temp0 -b data/gapped-in-hg19.bed|sort -k1,1 -k2,2n > temp001
+bedtools subtract -a temp001 -b data/gapped-in-both.bed|sort -k1,1 -k2,2n > temp002
+bedtools subtract -a temp002 -b data/blacklist.hg19.bed|sort -k1,1 -k2,2n > temp1
+rm temp0*
 
 ### filter gap in hg38 
 sort -k1,1 -k2,2n temp > temp01
 bedtools subtract -a temp01 -b hg19.between2un-gapped.bed  > temp2
 
 ## liftover
-liftOver temp2 hg19ToHg38.over.chain temp3 $outputunmap
+liftOver temp2 chainfile temp3 $outputunmap
 
 #### filter  duplicate, alt chromosome
 sort -k1,1 -k2,2n temp3|grep -E "chr(.|..)[[:blank:]]"|sort -k5,5|uniq -f4 -u|sort -k1,1 -k2,2n > temp002
@@ -32,22 +34,3 @@ awk '{if($5!~"_"){print $0}}' temp003|sort -k1,1 -k2,2n > $outputmap
 
 
 rm temp*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
