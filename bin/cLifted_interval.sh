af370cbe577a83e7de154e8fef3b682d16916c62
phuoc362 <phuoc362@gmail.com>
2019-06-04 00:29:29 +0700

    Update cLifted_interval.sh

--- bin/cLifted_interval.sh
+++ bin/cLifted_interval.sh
@@ -15,16 +15,16 @@ outputunmap=$4
 awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$1"."$2"."$3}' $bedpath| sort -k1,1 -k2,2n  >temp0
 
 # filtered blacklist and gap(gapped-in-hg19, gapped-in-both and blacklist)
-bedtools subtract -a temp0 -b data/gapped-in-hg19.bed|sort -k1,1 -k2,2n > temp001
-bedtools subtract -a temp001 -b data/gapped-in-both.bed|sort -k1,1 -k2,2n > temp002
-bedtools subtract -a temp002 -b data/blacklist.hg19.bed|sort -k1,1 -k2,2n > temp1
+bedtools subtract -a temp0 -b Lifted/data/gapped-in-hg19.bed|sort -k1,1 -k2,2n > temp001
+bedtools subtract -a temp001 -b Lifted/data/gapped-in-both.bed|sort -k1,1 -k2,2n > temp002
+bedtools subtract -a temp002 -b Lifted/data/blacklist.hg19.bed|sort -k1,1 -k2,2n > temp1
 rm temp0*
 ## liftover
 liftOver temp1 chainfile temp2 $outputunmap
 
 ### filter gap in hg38 
 sort -k1,1 -k2,2n temp2 > temp2.0
-bedtools intersect -a temp2.0 -b data/gapped-in-hg38.bed -v > temp3
+bedtools intersect -a temp2.0 -b Lifted/data/gapped-in-hg38.bed -v > temp3
 
 #### filter  duplicate, alt chromosome
 sort -k1,1 -k2,2n temp3|grep -E "chr(.|..)[[:blank:]]"|sort -k5,5|uniq -f4 -u|sort -k1,1 -k2,2n > temp3.1
