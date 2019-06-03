b690d38c50b2ed7612884de37099431ecf556460
phuoc362 <phuoc362@gmail.com>
2019-06-04 00:16:08 +0700

    Update clifted_CpG.sh

--- bin/clifted_CpG.sh
+++ bin/clifted_CpG.sh
@@ -16,7 +16,7 @@ outputunmap=$4
 awk '{print $1"\t"$2"\t"$3"\t"$4}' $bedpath| sort -k1,1 -k2,2n  >temp0
 
 # filtered blacklist and gap(gapped-in-hg19, gapped-in-both and blacklist)
-bedtools subtract -a temp0 -b data/gapped-in-hg19.bed|sort -k1,1 -k2,2n > temp001
+bedtools subtract -a temp0 -b data/hg19.gapped-in-hg38.bed|sort -k1,1 -k2,2n > temp001
 bedtools subtract -a temp001 -b data/gapped-in-both.bed|sort -k1,1 -k2,2n > temp002
 bedtools subtract -a temp002 -b data/blacklist.hg19.bed|sort -k1,1 -k2,2n > temp1
 rm temp0*
