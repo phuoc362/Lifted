3e48bbabdd47fab85d3e7a8c18ce49826d5b94d2
phuoc362 <phuoc362@gmail.com>
2019-06-04 01:45:06 +0700

    Update cLifted_interval.sh

--- bin/cLifted_interval.sh
+++ bin/cLifted_interval.sh
@@ -23,12 +23,12 @@ rm temp0*
 liftOver temp1 chainfile temp2 $outputunmap
 
 ### filter gap in hg38 
-sort -k1,1 -k2,2n temp2 > temp2.0
-bedtools intersect -a temp2.0 -b Lifted/data/gapped-in-hg38.bed -v > temp3
+sort -k1,1 -k2,2n temp2 > temp20
+bedtools intersect -a temp20 -b Lifted/data/gapped-in-hg38.bed -v > temp3
 
 #### filter  duplicate, alt chromosome
-sort -k1,1 -k2,2n temp3|grep -E "chr(.|..)[[:blank:]]"|sort -k5,5|uniq -f4 -u|sort -k1,1 -k2,2n > temp3.1
-bedtools merge -i  temp3.1 -c 5 -o collapse -delim "_" > temp3.2
+sort -k1,1 -k2,2n temp3|grep -E "chr(.|..)[[:blank:]]"|sort -k5,5|uniq -f4 -u|sort -k1,1 -k2,2n > temp31
+bedtools merge -i  temp3.1 -c 5 -o collapse -delim "_" > temp32
 awk '{if($5!~"_"){print $0}}' temp3.2|sort -k1,1 -k2,2n > $outputmap
 
 rm temp*
