9ae99d3a881ac9f60ead5ab37ec6bfed4f37b958
phuoc362 <phuoc362@gmail.com>
2019-06-04 00:33:13 +0700

    Update lLifted_interval.sh

--- bin/lLifted_interval.sh
+++ bin/lLifted_interval.sh
@@ -15,14 +15,14 @@ outputunmap=$4
 awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$1"."$2"."$3}' $bedpath| sort -k1,1 -k2,2n  >temp0
 
 # filtered blacklist and gap(gapped-in-hg19, gapped-in-both and blacklist)
-bedtools subtract -a temp0 -b data/gapped-in-hg19.bed|sort -k1,1 -k2,2n > temp001
-bedtools subtract -a temp001 -b data/gapped-in-both.bed|sort -k1,1 -k2,2n > temp002
-bedtools subtract -a temp002 -b data/blacklist.hg19.bed|sort -k1,1 -k2,2n > temp1
+bedtools subtract -a temp0 -b Lifted/data/gapped-in-hg19.bed|sort -k1,1 -k2,2n > temp001
+bedtools subtract -a temp001 -b Lifted/data/gapped-in-both.bed|sort -k1,1 -k2,2n > temp002
+bedtools subtract -a temp002 -b Lifted/data/blacklist.hg19.bed|sort -k1,1 -k2,2n > temp1
 rm temp0*
 
 ### filter gap in hg38 
 sort -k1,1 -k2,2n temp > temp01
-bedtools subtract -a temp01 -b hg19.between2un-gapped.bed  > temp2
+bedtools subtract -a temp01 -b Lifted/data/hg19.gapped-in-hg38.bed  > temp2
 
 ## liftover
 liftOver temp2 chainfile temp3 $outputunmap
