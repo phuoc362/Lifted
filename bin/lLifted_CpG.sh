82b759b339bc0875b675d46e4c62a51a6806ceb5
phuoc362 <phuoc362@gmail.com>
2019-06-04 10:14:00 +0700

    Update lLifted_CpG.sh

--- bin/lLifted_CpG.sh
+++ bin/lLifted_CpG.sh
@@ -17,7 +17,7 @@ rm temp*
 awk '{print $1"\t"$2"\t"$3"\t"$4}' $bedpath| sort -k1,1 -k2,2n  >temp0
 
 # filtered blacklist and gap(gapped-in-hg19, gapped-in-both and blacklist)
-bedtools subtract -a temp0 -b Lifted/data/hg19.gapped-in-hg38.bed|sort -k1,1 -k2,2n > temp001
+bedtools subtract -a temp0 -b Lifted/data/hg19-overlap-gapped-in-hg38.bed|sort -k1,1 -k2,2n > temp001
 bedtools subtract -a temp001 -b Lifted/data/gapped-in-hg19.bed|sort -k1,1 -k2,2n > temp002
 bedtools subtract -a temp002 -b Lifted/data/gapped-in-both.bed|sort -k1,1 -k2,2n > temp003
 bedtools subtract -a temp003 -b Lifted/data/blacklist.hg19.bed|sort -k1,1 -k2,2n > temp
@@ -25,9 +25,9 @@ rm temp0*
 ## liftover
 liftOver temp $chainfile temp2 $outputunmap
 
-### filter gap in hg38, not cg, duplicates
+### filter  not cg, duplicates
 sort -k1,1 -k2,2n temp2 > temp20
-cat Lifted/data/notCG.bed Lifted/data/gapped-in-hg38.bed Lifted/data/duplication.bed|sort -k1,1 -k2,2n|awk '{print $1"\t"$2"\t"$3}' > temp21
+cat Lifted/data/notCG.bed Lifted/data/duplication.bed|sort -k1,1 -k2,2n|awk '{print $1"\t"$2"\t"$3}' > temp21
 bedtools intersect -a temp20 -b temp21 -v > temp3
 
 #### filter alt chr
